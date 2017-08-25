2d3817a300e39703108e1de5f122e76ec9a31f87
Shane Boland <sboland926@gmail.com>
2017-08-25 19:04:34 -0400

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,56 @@
+require 'users_helper'
+
+class User < ActiveRecord::Base
+  belongs_to :referrer, class_name: 'User', foreign_key: 'referrer_id'
+  has_many :referrals, class_name: 'User', foreign_key: 'referrer_id'
+
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i,
+    message: 'Invalid email format.'
+  }
+  validates :referral_code, uniqueness: true
+
+  before_create :create_referral_code
+  after_create :send_welcome_email
+
+  REFERRAL_STEPS = [
+    {
+      'count' => 1,
+      'html' => '10&#37; Off Your First Kit',
+      'class' => 'two',
+      'image' =>  ActionController::Base.helpers.asset_path(
+        'refer/cream-tooltip@2x.png')
+    },
+    {
+      'count' => 3,
+      'html' => '50&#37; Off Your First Kit',
+      'class' => 'three',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/truman@2x.png')
+    },
+    {
+      'count' => 5,
+      'html' => 'Free Kit',
+      'class' => 'four',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/winston@2x.png')
+    },
+    {
+      'count' => 10,
+      'html' => 'Three Free Kits',
+      'class' => 'five',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/blade-explain@2x.png')
+    }
+  ]
+
+  private
+
+  def create_referral_code
+    self.referral_code = UsersHelper.unused_referral_code
+  end
+
+  def send_welcome_email
+    UserMailer.delay.signup_email(self)
+  end
+end
