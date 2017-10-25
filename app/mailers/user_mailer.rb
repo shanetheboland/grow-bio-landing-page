fb7a98ec1ebc5fe3afd3c623a0ae69801cc9a8d8
Shane Boland <sboland926@gmail.com>
2017-10-25 14:54:52 -0400

    Adds further explanation section with links to legacy kits

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "GROW.bio <shane@grow.bio>"
+  default from: "GrowBio <shane@grow.bio>"
 
   def signup_email(user)
     @user = user
     @twitter_message = "Biomaterial kits, tutorials, and more! Excited for #growbio to launch."
 
-    mail(:to => user.email, :subject => "Thanks for signing up! | GROW.bio")
+    mail(:to => user.email, :subject => "Thanks for signing up! | GrowBio")
   end
 end
